fbb64c404a284c5b3a7e6aecb7d854aadce9dfe1
nethaji <nethajic9@gmail.com>
2015-09-02 14:49:42 +0530

    checkModelStatus method changed

--- modules/integration/tests-integration/tests-service/src/test/java/org/wso2/carbon/ml/lifecycle/test/Dataset8DASDataTestCase.java
+++ modules/integration/tests-integration/tests-service/src/test/java/org/wso2/carbon/ml/lifecycle/test/Dataset8DASDataTestCase.java
@@ -90,7 +90,7 @@ public class Dataset8DASDataTestCase extends MLBaseTest {
                 .getStatusCode());
         response.close();
         // Waiting for model building to end
-        boolean status = MLTestUtils.checkModelStatus(modelName, mlHttpclient,
+        boolean status = MLTestUtils.checkModelStatusCompleted(modelName, mlHttpclient,
                 MLIntegrationTestConstants.THREAD_SLEEP_TIME_LARGE, 1000);
         // Checks whether model building completed successfully
         assertEquals("Model building did not complete successfully", true, status);
