bb78d910f19af21cb9141bcb28256d01a22446e6
cdathuraliya <cdathuraliya@gmail.com>
2015-09-03 16:38:39 +0530

    Fixing https://wso2.org/jira/browse/ML-229

--- modules/integration/tests-common/test-utils/src/main/java/org/wso2/carbon/ml/integration/common/utils/MLBaseTest.java
+++ modules/integration/tests-common/test-utils/src/main/java/org/wso2/carbon/ml/integration/common/utils/MLBaseTest.java
@@ -24,6 +24,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -213,7 +214,7 @@ public abstract class MLBaseTest {
      * @throws JSONException
      */
     private static int getId(CloseableHttpResponse response) throws IOException, JSONException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), StandardCharsets.UTF_8));
         JSONObject responseJson = new JSONObject(bufferedReader.readLine());
         bufferedReader.close();
         response.close();
