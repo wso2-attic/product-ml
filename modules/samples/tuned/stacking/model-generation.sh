e3e9c92f75fa0debc928963c20417f0f859c4025
Misgana <misgananegassi@gmail.com>
2016-06-30 10:13:10 +0200

    removed white spaces

--- modules/samples/tuned/stacking/model-generation.sh
+++ modules/samples/tuned/stacking/model-generation.sh
@@ -57,10 +57,10 @@ sleep 2
 
 echo "#setting tuned hyper params"
 curl -X POST -d @'hyper-parameters' -H "Content-Type: application/json" -H "Authorization: Basic YWRtaW46YWRtaW4=" -v https://$SEVER_IP:9443/api/analyses/${analysisId}/hyperParams?algorithmName=STACKING -k -v
-curl -X POST -d @'hyper-parameters' -H "Content-Type: application/json" -H "Authorization: Basic YWRtaW46YWRtaW4=" -v https://$SEVER_IP:9443/api/analyses/${analysisId}/hyperParams?algorithmName= Name_of_Base_Algorithm_0 -k -v
-curl -X POST -d @'hyper-parameters' -H "Content-Type: application/json" -H "Authorization: Basic YWRtaW46YWRtaW4=" -v https://$SEVER_IP:9443/api/analyses/${analysisId}/hyperParams?algorithmName= Name_of_Base_Algorithm_1 -k -v
-curl -X POST -d @'hyper-parameters' -H "Content-Type: application/json" -H "Authorization: Basic YWRtaW46YWRtaW4=" -v https://$SEVER_IP:9443/api/analyses/${analysisId}/hyperParams?algorithmName= Name_of_Base_Algorithm_2 -k -v
-curl -X POST -d @'hyper-parameters' -H "Content-Type: application/json" -H "Authorization: Basic YWRtaW46YWRtaW4=" -v https://$SEVER_IP:9443/api/analyses/${analysisId}/hyperParams?algorithmName= Name_of_Meta_Algorithm -k -v
+#curl -X POST -d @'hyper-parameters' -H "Content-Type: application/json" -H "Authorization: Basic YWRtaW46YWRtaW4=" -v https://$SEVER_IP:9443/api/analyses/${analysisId}/hyperParams?algorithmName=Name_of_Base_Algorithm_0-k -v
+#curl -X POST -d @'hyper-parameters' -H "Content-Type: application/json" -H "Authorization: Basic YWRtaW46YWRtaW4=" -v https://$SEVER_IP:9443/api/analyses/${analysisId}/hyperParams?algorithmName=Name_of_Base_Algorithm_1 -k -v
+#curl -X POST -d @'hyper-parameters' -H "Content-Type: application/json" -H "Authorization: Basic YWRtaW46YWRtaW4=" -v https://$SEVER_IP:9443/api/analyses/${analysisId}/hyperParams?algorithmName=Name_of_Base_Algorithm_2 -k -v
+#curl -X POST -d @'hyper-parameters' -H "Content-Type: application/json" -H "Authorization: Basic YWRtaW46YWRtaW4=" -v https://$SEVER_IP:9443/api/analyses/${analysisId}/hyperParams?algorithmName=Name_of_Meta_Algorithm -k -v
 
 sleep 2
 
